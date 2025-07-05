4c6f81be48ba05d3f8bf706d2bf8db8f6ae40d4f
nesmanpro <nesmanpro@gmail.com>
2025-07-05 19:35:45 +0200

    fixing iphone animations

--- src/components/atoms/textAnimate.jsx
+++ src/components/atoms/textAnimate.jsx
@@ -63,7 +63,7 @@ export default function TextAnimate({ children, className }) {
             </span>
             <span
                 ref={inRef}
-                className="absolute inset-0 opacity-60 translate-y-full capitalize"
+                className="absolute inset-0 opacity-60 translate-y-full"
             >
                 {children}
             </span>
