3144113819c17089ac70a712192a252bd4f6e22e
nesmanpro <nesmanpro@gmail.com>
2025-07-31 18:55:52 +0200

    feat. work: custom cursor, works img updated

---
+++ src/components/molecules/CustomCursor.jsx
@@ -0,0 +1,40 @@
+import { useRef } from "react"
+import { useGSAP } from "@gsap/react"
+import gsap from "gsap"
+
+export const CustomCursor = ({ x, y, visible }) => {
+
+    const cursorRef = useRef(null)
+    const textRef = useRef(null)
+
+
+
+
+    useGSAP(() => {
+        let tl = gsap.timeline({ defaults: { duration: 0.2, ease: 'bounce.inOut' } });
+
+        if (visible) {
+            tl.to(cursorRef.current, {
+                scale: 1, opacity: 1
+            })
+        } else {
+            tl.to(cursorRef.current, { scale: 0, opacity: 0 })
+
+        }
+    }, { dependencies: [visible] })
+
+
+    return (
+        <div
+            ref={cursorRef}
+            className="fixed size-[120px] z-100 bg-yellow-50 rounded-full flex justify-center items-center -translate-x-1/2 -translate-y-1/2 pointer-events-none scale-0"
+            style={{ left: x, top: y }}
+        >
+            <h3
+                ref={textRef}
+                className="font-bold text-lg">
+                Let's see!
+            </h3>
+        </div>
+    )
+}
--- src/sections/Works.jsx
+++ src/sections/Works.jsx
@@ -1,49 +1,106 @@
 import { useGSAP } from "@gsap/react"
-import { useRef } from "react"
+import { useRef, useState } from "react"
 import ScrollTrigger from "gsap/ScrollTrigger"
 import gsap from "gsap"
 import SectionLayout from "../components/molecules/SectionLayout"
+import { CustomCursor } from "../components/molecules/CustomCursor"
+import useMousePosition from "../hooks/animations/useMousePosition"
 
 gsap.registerPlugin(ScrollTrigger, useGSAP)
 
 const worksItems = [
     {
-        title: 'Entuitive Engineering',
-        subtitle: 'Technology & Arts',
-        categories: ['Design', 'Development'],
+        id: '1',
+        title: 'Cycling Class',
+        subtitle: 'React, Vite, Tailwind',
+        categories: ['UI-UX', 'Development'],
         year: '2024',
-        imgUrl: '/assets/10.jpg',
-        bgImg: '/assets/11.jpg'
+        imgUrl: '/img/img-0.webp',
+        bgImg: '/assets/14.jpg',
+        url: 'https://ecommerce-roque.vercel.app/'
     },
     {
-        title: 'SOGAI',
-        subtitle: 'Architecture & Engineering',
-        categories: ['Design', 'Development'],
+        id: '2',
+        title: 'Ecommerce Filters',
+        subtitle: 'Vanilla JS, ',
+        categories: ['Testing', 'Development'],
         year: '2025',
-        imgUrl: '/assets/13.jpg',
-        bgImg: '/assets/11.jpg'
+        imgUrl: '/img/img-2.webp',
+        bgImg: '/assets/15.jpg',
+        url: 'https://avoris-ptech-waveless.vercel.app/'
     },
     {
+        id: '3',
         title: 'BlueBrown Partners',
         subtitle: 'Venture Capital',
         categories: ['Design', 'branding'],
         year: '2024',
-        imgUrl: '/assets/3.jpg',
-        bgImg: '/assets/10.jpg'
+        imgUrl: '/img/img-3.webp',
+        bgImg: '/assets/12.jpg',
+        url: ''
     },
     {
+        id: '4',
         title: "Do the right things!",
         subtitle: 'Artist Atelier',
         categories: ['photography', 'branding'],
         year: '2025',
-        imgUrl: '/assets/2.jpg',
-        bgImg: '/assets/15.jpg'
+        imgUrl: '/img/img-4.webp',
+        bgImg: '/assets/14.jpg',
+        url: ''
+    },
+    {
+        id: '5',
+        title: "Dev Portfolio",
+        subtitle: 'NextJS, Typescript, Tailwind, Framer Motion',
+        categories: ['UI-UX', 'Developing'],
+        year: '2025',
+        imgUrl: '/img/img-5.webp',
+        bgImg: '/assets/14.jpg',
+        url: 'https://github.com/nesmanpro/dev_Portfolio'
+    },
+    {
+        id: '6',
+        title: "Nae Riviera Maya",
+        subtitle: 'React, Vite, Tailwind',
+        categories: ['UI-UX', 'Developing'],
+        year: '2025',
+        imgUrl: '/img/img-6.webp',
+        bgImg: '/assets/14.jpg',
+        url: 'https://www.naerivieramaya.com/'
+    },
+    {
+        id: '7',
+        title: 'Web Intro Animated ',
+        subtitle: 'React, GSAP, NextJS',
+        categories: ['UI-UX', 'Development'],
+        year: '2024',
+        imgUrl: '/img/img-1.webp',
+        bgImg: '/assets/14.jpg',
+        url: ''
+    },
+    {
+        id: '8',
+        title: 'NoName Club Intro',
+        subtitle: 'React, GSAP, NextJS',
+        categories: ['UI-UX', 'Development'],
+        year: '2024',
+        imgUrl: '/img/img-7.webp',
+        bgImg: '/assets/14.jpg',
+        url: 'https://js-vanilla-gsap-animations.vercel.app/'
     },
 ]
 
 export default function Works() {
+    const { x, y } = useMousePosition()
     const contRef = useRef(null)
     const numbersRef = useRef(null)
+
+    const [cursorVisible, setCursorVisible] = useState(false)
+
+    const handleMouseEnter = () => setCursorVisible(true)
+    const handleMouseLeave = () => setCursorVisible(false)
+
 
     useGSAP(() => {
         const containers = gsap.utils.toArray('[data-container-index]')
@@ -75,6 +132,8 @@ export default function Works() {
 
     }, { scope: contRef })
 
+
+
     return (
 
         <SectionLayout
@@ -85,29 +144,37 @@ export default function Works() {
             description="Featured projects that have been meticulously crafted with passion to drive results and impact."
         >
 
-
-
             <div ref={contRef} className="grid grid-cols-12 pt-space-lg gap-space-md h-full">
                 <div className="sticky top-12 col-span-5 hidden h-fit w-full overflow-hidden text-[22vw] font-normal leading-[0.8] md:flex">
                     <span className="relative">0</span>
                     <span className="relative">
                         <div ref={numbersRef} className="absolute left-0 flex flex-col">
                             {worksItems.map((work, idx) => (
-                                <span key={work.title} data-number-index={idx} className={`left-0`}>{idx + 1}.</span>
+                                <span key={work.id} data-number-index={idx} className={`left-0`}>{idx + 1}.</span>
                             ))}
                         </div>
                     </span>
                 </div>
                 <aside className="col-span-12 flex flex-col gap-y-space-xl  md:col-span-7 md:gap-y-space-2xl">
-                    {worksItems.map((work, index) => (
-                        <div key={work.title} data-container-index={index} className="@container">
-                            <div className="relative flex aspect-square items-center justify-center overflow-clip rounded-md p-space-md md:p-space-lg xl:p-space-2xl">
 
+                    {worksItems.map((work, index) => (
+                        <div
+                            key={work.id}
+                            data-container-index={index}
+                            className="@container"
+                        >
+                            <a
+                                href={work.url}
+                                target="_blank"
+                                onMouseEnter={handleMouseEnter}
+                                onMouseLeave={handleMouseLeave}
+                                className="flex aspect-square items-center justify-center overflow-clip rounded-md p-space-md md:p-space-lg xl:p-space-2xl cursor-none relative">
                                 <img loading="lazy" className="absolute w-full h-full object-cover object-top" src={work.bgImg} alt={work.title} />
                                 <div className="w-full overflow-clip z-0 aspect-[4/3] rounded-md">
                                     <img loading="lazy" className="w-full h-full object-cover object-top" src={work.imgUrl} alt={work.title} />
                                 </div>
-                            </div>
+
+                            </a>
                             <div className="flex flex-col justify-between gap-y-space-sm pt-space-xs @lg:flex-row">
                                 <div className="flex flex-col gap-y-space-3xs">
                                     <span className="font-mono text-sm text-gray-950/50">{work.subtitle}</span>
@@ -126,6 +193,8 @@ export default function Works() {
                     ))}
                 </aside>
             </div>
+
+            <CustomCursor x={x} y={y} visible={cursorVisible} />
         </SectionLayout>
 
     )
